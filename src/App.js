a435ea775a20cfba8cdc49ea834b6cbcd1959011
lenayoo <lenayoo17@gmail.com>
2024-02-23 14:46:16 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="flex justify-center p-6 pb-10 font-mono">
+       hello
+      </header>
+    </div>
+  );
+}
+
+export default App;
